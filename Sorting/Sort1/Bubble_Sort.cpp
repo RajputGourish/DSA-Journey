2434b2cb8282bcd2b268f15e6c9b8ea75877dd61
RajputGourish <gourishrajput000@gmail.com>
2025-03-19 01:28:31 +0530

    BUbble sort solution

--- Sorting/Sort1/Bubble_Sort.cpp
+++ Sorting/Sort1/Bubble_Sort.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*
+    Problem link ->
+            https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort
+*/
+
 // Printing the array
 void print(vector<int> &arr)
 {
@@ -10,8 +15,27 @@ void print(vector<int> &arr)
     }
     cout << endl;
 }
+
+// Sorting the array
 void Bubble_Sort(vector<int> &arr)
 {
+    int n = arr.size(); // size of the array
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1]) // swap the value if left is greater than right
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Time Complexity = O(N^2)
+    // Space Complexity = O(1)
 }
 int main()
 {
