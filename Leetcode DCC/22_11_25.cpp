6cc0077d365b566ff267d4025615b9eaf1f355a3
RajputGourish <gourishrajput000@gmail.com>
2025-11-23 08:15:32 +0530

    updation link for problem

--- Leetcode DCC/22_11_25.cpp
+++ Leetcode DCC/22_11_25.cpp
@@ -2,6 +2,12 @@
 #include<vector>
 using namespace std;
 
+/*
+    Problem Link ->
+                https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/?envType=daily-question&envId=2025-11-22
+
+*/
+
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
