449adea7acc048fee220639ef3328efe48fbb78b
RajputGourish <gourishrajput000@gmail.com>
2026-06-17 21:15:30 +0530

    POTD

--- Leetcode DCC/17_06_26.cpp
+++ Leetcode DCC/17_06_26.cpp
@@ -34,18 +34,71 @@ char processStr(string s, long long k)
             ans += c;
         }
 
-        if(ans.size() >= k+1) return ans[k];
+        if (ans.size() >= k + 1)
+            return ans[k];
     }
 
-    return (ans.size() < k+1) ? '.' : ans[k];
+    return (ans.size() < k + 1) ? '.' : ans[k];
 
     // MLE => Memory Limit Exceeds
+}
+
+char processStr2(string s, long long k)
+{
+    long long len = 0;
+
+    for (char c : s)
+    {
+        if (c == '*')
+        {
+            len = max(0LL, len - 1);
+        }
+        else if (c == '#')
+        {
+            len *= 2;
+        }
+        else if (c != '%')
+        {
+            len++;
+        }
+    }
+
+    if (k >= len)
+        return '.';
+
+    for (int i = (int)s.size() - 1; i >= 0; --i)
+    {
+        char c = s[i];
+
+        if (c == '*')
+        {
+            len++;
+        }
+        else if (c == '#')
+        {
+            len /= 2;
+            if (k >= len)
+                k -= len;
+        }
+        else if (c == '%')
+        {
+            k = len - 1 - k;
+        }
+        else
+        {
+            len--;
+            if (k == len)
+                return c;
+        }
+    }
+
+    return '.';
 }
 
 int main()
 {
     string s = "z*#";
-    cout<<processStr(s, 0)<<endl;
-    
+    cout << processStr(s, 0) << endl;
+
     return 0;
 }
